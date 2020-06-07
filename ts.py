2a20d3172a0bf495d1291a458fc87a0fe9e8fa2e
jason <5518263@qq.com>
2020-06-07 15:14:07 +0800

    修改

--- ts.py
+++ ts.py
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-a = 1
-print(id(a),id(1))
-
-def fun(a):
-    print(id(a))
-    a = 2
-    print(id(a),id(2))
-
-fun(a)
-
-print(id(a))
-print(a)
-=======
 a=1
 b=2
 if a==1:
@@ -26,4 +12,3 @@ if a==1:
 
 if b==2:
     print('OK')
->>>>>>> 08f4499315c9f9a71cede7b77f3a3fb599301cf0
